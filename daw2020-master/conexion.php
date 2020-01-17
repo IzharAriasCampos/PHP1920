be85af05808c0f4250c6086c1428bb0e18cae38f
Kaysune <59911239+Kaysune@users.noreply.github.com>
2020-01-17 13:43:10 +0100

    Update conexion.php

--- daw2020-master/conexion.php
+++ daw2020-master/conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '10.130.8.173');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'comprasweb');
+define('DB_DATABASE', 'COMPRASWEB');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
